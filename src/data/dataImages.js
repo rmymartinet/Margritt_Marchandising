d1cfa9431a10541708be634ce5a840810d40189b
martinet <remymartinet.contact@gmail.com>
2024-09-23 11:55:01 +0200

    Updated data and dataImages files.

--- src/data/dataImages.js
+++ src/data/dataImages.js
@@ -2,140 +2,136 @@
 Images WEBP
 -------------- */
 
-import bibulle from "../assets/images/Gallery/Originaux_120/dimension_1500/webp/bibulle!.webp";
-import bibulle1 from "../assets/images/Gallery/Originaux_120/dimension_1500/webp/bibulle1.webp";
-import bibulle2 from "../assets/images/Gallery/Originaux_120/dimension_1500/webp/bibulle3bis.webp";
-import futurama1 from "../assets/images/Gallery/Originaux_120/dimension_1500/webp/futurama1.webp";
-import MaxiBibulle from "../assets/images/Gallery/Originaux_120/dimension_1500/webp/maxi-bibulle.webp";
-import mecaniqueDesReves from "../assets/images/Gallery/Originaux_120/dimension_1500/webp/mecanique.webp";
+/*------------
+Projects
+-------------- */
+import project1 from "../assets/images/Project/p1.jpg";
+import project2 from "../assets/images/Project/p2.jpg";
+import project3 from "../assets/images/Project/p3.jpg";
 
-import bibulle3Moyen from "../assets/images/Gallery/Tirage_50/dimension_1500/webp/bibulle3.webp";
-import bibulle5Moyen from "../assets/images/Gallery/Tirage_50/dimension_1500/webp/bibulle5.webp";
-import futurama2Moyen from "../assets/images/Gallery/Tirage_50/dimension_1500/webp/futurama2.webp";
-import futurama3Moyen from "../assets/images/Gallery/Tirage_50/dimension_1500/webp/futurama3.webp";
+/*------------
+Gallery
+-------------- */
+import maxiBibulleLargeWebp from "../assets/images/Gallery/Desktop/Gallery/webp/maxi-bibulle.webp";
+import mecaniqueDesRevesLargeWebp from "../assets/images/Gallery/Desktop/Gallery/webp/mecanique.webp";
 
-import bibulle3_1 from "../assets/images/Gallery/Tirage_50/bibulle3_details/bibulle3-1.webp";
-import bibulle3_2 from "../assets/images/Gallery/Tirage_50/bibulle3_details/bibulle3-2.webp";
-import bibulle3_3 from "../assets/images/Gallery/Tirage_50/bibulle3_details/bibulle3-3.webp";
-import bibulle3_4 from "../assets/images/Gallery/Tirage_50/bibulle3_details/bibulle3-4.webp";
+/*------------
+Originals
+-------------- */
+import bibulle1LargeWebp from "../assets/images/Gallery/Desktop/Originals/webp/bibulle1.webp";
+import bibulle2LargeWebp from "../assets/images/Gallery/Desktop/Originals/webp/bibulle2.webp";
+import bibulle3LargeWebp from "../assets/images/Gallery/Desktop/Originals/webp/bibulle3.webp";
+import bibulle5LargeWebp from "../assets/images/Gallery/Desktop/Originals/webp/bibulle5.webp";
+import futurama1LargeWebp from "../assets/images/Gallery/Desktop/Originals/webp/futurama1.webp";
+import futurama2LargeWebp from "../assets/images/Gallery/Desktop/Originals/webp/futurama2.webp";
+import futurama3LargeWebp from "../assets/images/Gallery/Desktop/Originals/webp/futurama3.webp";
 
-import bibulle5_1 from "../assets/images/Gallery/Tirage_50/bibulle5_details/webp/bibulle5-1.webp";
-import bibulle5_2 from "../assets/images/Gallery/Tirage_50/bibulle5_details/webp/bibulle5-2.webp";
-import bibulle5_3 from "../assets/images/Gallery/Tirage_50/bibulle5_details/webp/bibulle5-3.webp";
-import bibulle5_4 from "../assets/images/Gallery/Tirage_50/bibulle5_details/webp/bibulle5-4.webp";
+/*------------
+Details
+-------------- */
+import bibulle3_1LargeWebp from "../assets/images/Gallery/Desktop/Tirages/bibulle3_details/webp/bibulle3-1.webp";
+import bibulle3_2LargeWebp from "../assets/images/Gallery/Desktop/Tirages/bibulle3_details/webp/bibulle3-2.webp";
+import bibulle3_3LargeWebp from "../assets/images/Gallery/Desktop/Tirages/bibulle3_details/webp/bibulle3-3.webp";
+import bibulle3_4LargeWebp from "../assets/images/Gallery/Desktop/Tirages/bibulle3_details/webp/bibulle3-4.webp";
 
-import futurama2_1 from "../assets/images/Gallery/Tirage_50/futurama2_details/futurama2-1.webp";
-import futurama2_2 from "../assets/images/Gallery/Tirage_50/futurama2_details/futurama2-2.webp";
-import futurama2_3 from "../assets/images/Gallery/Tirage_50/futurama2_details/futurama2-3.webp";
-import futurama2_4 from "../assets/images/Gallery/Tirage_50/futurama2_details/futurama2-4.webp";
+import bibulle5_1LargeWebp from "../assets/images/Gallery/Desktop/Tirages/bibulle5_details/webp/bibulle5-1.webp";
+import bibulle5_2LargeWebp from "../assets/images/Gallery/Desktop/Tirages/bibulle5_details/webp/bibulle5-2.webp";
+import bibulle5_3LargeWebp from "../assets/images/Gallery/Desktop/Tirages/bibulle5_details/webp/bibulle5-3.webp";
+import bibulle5_4LargeWebp from "../assets/images/Gallery/Desktop/Tirages/bibulle5_details/webp/bibulle5-4.webp";
 
-import futurama3_1 from "../assets/images/Gallery/Tirage_50/futurama3_details/futurama3-1.webp";
-import futurama3_2 from "../assets/images/Gallery/Tirage_50/futurama3_details/futurama3-2.webp";
-import futurama3_3 from "../assets/images/Gallery/Tirage_50/futurama3_details/futurama3-3.webp";
-import futurama3_4 from "../assets/images/Gallery/Tirage_50/futurama3_details/futurama3-4.webp";
+import futurama2_1LargeWebp from "../assets/images/Gallery/Desktop/Tirages/futurama2_details/webp/futurama2-1.webp";
+import futurama2_2LargeWebp from "../assets/images/Gallery/Desktop/Tirages/futurama2_details/webp/futurama2-2.webp";
+import futurama2_3LargeWebp from "../assets/images/Gallery/Desktop/Tirages/futurama2_details/webp/futurama2-3.webp";
+import futurama2_4LargeWebp from "../assets/images/Gallery/Desktop/Tirages/futurama2_details/webp/futurama2-4.webp";
+
+import futurama3_1LargeWebp from "../assets/images/Gallery/Desktop/Tirages/futurama3_details/webp/futurama3-1.webp";
+import futurama3_2LargeWebp from "../assets/images/Gallery/Desktop/Tirages/futurama3_details/webp/futurama3-2.webp";
+import futurama3_3LargeWebp from "../assets/images/Gallery/Desktop/Tirages/futurama3_details/webp/futurama3-3.webp";
+import futurama3_4LargeWebp from "../assets/images/Gallery/Desktop/Tirages/futurama3_details/webp/futurama3-4.webp";
+
+/*------------
+About
+-------------- */
 
 import about1 from "../assets/images/about/webp/about1.webp";
 import about2 from "../assets/images/about/webp/about2.webp";
 
 /*------------
-Images JPG
+Exports
 -------------- */
-
-import bibulleJpg from "../assets/images/Gallery/Originaux_120/dimension_1500/bibulle!.jpg";
-import bibulle1Jpg from "../assets/images/Gallery/Originaux_120/dimension_1500/bibulle1.jpg";
-import bibulle2Jpg from "../assets/images/Gallery/Originaux_120/dimension_1500/bibulle3bis.jpg";
-import futurama1Jpg from "../assets/images/Gallery/Originaux_120/dimension_1500/futurama1.jpg";
-import MaxiBibulleJpg from "../assets/images/Gallery/Originaux_120/dimension_1500/maxi-bibulle.jpeg";
-import mecaniqueDesRevesJpg from "../assets/images/Gallery/Originaux_120/dimension_1500/mecanique.jpeg";
-
-import bibulle3MoyenJpg from "../assets/images/Gallery/Tirage_50/dimension_1500/bibulle3.jpg";
-import bibulle5MoyenJpg from "../assets/images/Gallery/Tirage_50/dimension_1500/bibulle5.jpg";
-import futurama2MoyenJpg from "../assets/images/Gallery/Tirage_50/dimension_1500/futurama2.jpg";
-import futurama3MoyenJpg from "../assets/images/Gallery/Tirage_50/dimension_1500/futurama3.jpeg";
-
-import about1Jpg from "../assets/images/about/about1.jpg";
-import about2Jpg from "../assets/images/about/about2.jpg";
 
 export const originalBibulle = {
   webp: {
-    bibulle1: bibulle1,
-    bibulle2: bibulle2,
-    bibulle3: bibulle3Moyen,
-    bibulle5: bibulle5Moyen,
-    bibulleExclamation: bibulle,
-    maxiBibulle: MaxiBibulle,
-  },
-  jpg: {
-    bibulle1: bibulle1Jpg,
-    bibulle2: bibulle2Jpg,
-    bibulle3: bibulle3MoyenJpg,
-    bibulle5: bibulle5MoyenJpg,
-    bibulleExclamation: bibulleJpg,
-    maxiBibulle: MaxiBibulleJpg,
+    bibulle1: bibulle1LargeWebp,
+    bibulle2: bibulle2LargeWebp,
+    bibulle3: bibulle3LargeWebp,
+    bibulle5: bibulle5LargeWebp,
+    maxiBibulle: maxiBibulleLargeWebp,
   },
 };
 
 export const originalFuturama = {
   webp: {
-    futurama1: futurama1,
-    futurama2: futurama2Moyen,
-    futurama3: futurama3Moyen,
-  },
-  jpg: {
-    futurama1: futurama1Jpg,
-    futurama2: futurama2MoyenJpg,
-    futurama3: futurama3MoyenJpg,
+    futurama1: futurama1LargeWebp,
+    futurama2: futurama2LargeWebp,
+    futurama3: futurama3LargeWebp,
   },
 };
 
 export const mecaniqueDesRevesOriginal = {
   webp: {
-    mecaniqueDesReves: mecaniqueDesReves,
+    mecaniqueDesReves: mecaniqueDesRevesLargeWebp,
   },
-  jpg: {
-    mecaniqueDesReves: mecaniqueDesRevesJpg,
+};
+
+export const maxiBibulleOriginal = {
+  webp: {
+    maxiBibulle: maxiBibulleLargeWebp,
+  },
+};
+
+export const projectData = {
+  p1: {
+    img1: project1,
+    img2: project2,
+    img3: project3,
   },
 };
 
 export const tirageBibulle3 = {
-  bibulle3_1: bibulle3_1,
-  bibulle3_2: bibulle3_2,
-  bibulle3_3: bibulle3_3,
-  bibulle3_4: bibulle3_4,
+  bibulle3_1: bibulle3_1LargeWebp,
+  bibulle3_2: bibulle3_2LargeWebp,
+  bibulle3_3: bibulle3_3LargeWebp,
+  bibulle3_4: bibulle3_4LargeWebp,
 };
 
 export const tirageBibulle5 = {
-  bibulle5_1: bibulle5_1,
-  bibulle5_2: bibulle5_2,
-  bibulle5_3: bibulle5_3,
-  bibulle5_4: bibulle5_4,
+  bibulle5_1: bibulle5_1LargeWebp,
+  bibulle5_2: bibulle5_2LargeWebp,
+  bibulle5_3: bibulle5_3LargeWebp,
+  bibulle5_4: bibulle5_4LargeWebp,
 };
 
 export const tirageFuturama2 = {
-  futurama2_1: futurama2_1,
-  futurama2_2: futurama2_2,
-  futurama2_3: futurama2_3,
-  futurama2_4: futurama2_4,
+  futurama2_1: futurama2_1LargeWebp,
+  futurama2_2: futurama2_2LargeWebp,
+  futurama2_3: futurama2_3LargeWebp,
+  futurama2_4: futurama2_4LargeWebp,
 };
 
 export const tirageFuturama3 = {
-  futurama3_1: futurama3_1,
-  futurama3_2: futurama3_2,
-  futurama3_3: futurama3_3,
-  futurama3_4: futurama3_4,
+  futurama3_1: futurama3_1LargeWebp,
+  futurama3_2: futurama3_2LargeWebp,
+  futurama3_3: futurama3_3LargeWebp,
+  futurama3_4: futurama3_4LargeWebp,
 };
 
 /*------------
 Images About
 -------------- */
-
 export const about = {
   webp: {
     about1: about1,
     about2: about2,
-  },
-  jpg: {
-    about1: about1Jpg,
-    about2: about2Jpg,
   },
 };
