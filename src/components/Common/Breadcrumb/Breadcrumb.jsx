680a2b930210d87c31d2a77bd3f86af34a7ed5cd
martinet <remymartinet.contact@gmail.com>
2024-09-23 11:55:40 +0200

    Updated Breadcrumb component to dynamically display path segments.

--- src/components/Common/Breadcrumb/Breadcrumb.jsx
+++ src/components/Common/Breadcrumb/Breadcrumb.jsx
@@ -4,34 +4,50 @@ import "./Breadcrumb.scss";
 
 const Breadcrumb = () => {
   const location = useLocation();
-  const pathnames = location.pathname.split("/").filter((x) => x);
+  let pathnames = location.pathname.split("/").filter((x) => x);
+
+  // Si le chemin contient au moins deux segments, utiliser le premier et le dernier segment
+  if (pathnames.length > 1) {
+    pathnames = [pathnames[0], pathnames[pathnames.length - 1]];
+  }
 
   return (
     <nav aria-label="breadcrumb">
       <ol className="breadcrumb">
+        {/* Home */}
         <li className="breadcrumb-item">
           <Link to="/">Home</Link>
         </li>
-        {pathnames.map((value, index) => {
-          const to = `/${pathnames.slice(0, index + 1).join("/")}`;
-          const isLast = index === pathnames.length - 1;
 
-          return (
-            <span key={to}>
-              <span style={{ margin: "0 5px", opacity: "0.5" }}>/</span>
-              {isLast ? (
-                ""
-              ) : (
-                <Link to={to} className="breadcrumb-item">
-                  {value.charAt(0).toUpperCase() + value.slice(1)}
-                </Link>
-              )}
-            </span>
-          );
-        })}
+        {/* Dynamically display the first and last path segments */}
       </ol>
     </nav>
   );
 };
 
 export default Breadcrumb;
+
+// {
+//   pathnames.map((value, index) => {
+//     const to = index === 1 ? `/tirages/${value}` : `/${value}`;
+//     const isLast = index === pathnames.length - 1;
+
+//     return (
+//       <span key={to}>
+//         {/* Separator */}
+//         <span style={{ margin: "0 5px", opacity: "0.5" }}>/</span>
+
+//         {/* Render the breadcrumb with a link for non-last items */}
+//         {isLast ? (
+//           <span className="breadcrumb-item active">
+//             {value.charAt(0).toUpperCase() + value.slice(1).replace("-", " ")}
+//           </span>
+//         ) : (
+//           <Link to={to} className="breadcrumb-item">
+//             {value.charAt(0).toUpperCase() + value.slice(1).replace("-", " ")}
+//           </Link>
+//         )}
+//       </span>
+//     );
+//   });
+// }
