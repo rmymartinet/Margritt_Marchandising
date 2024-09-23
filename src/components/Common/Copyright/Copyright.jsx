332c5d4f652fe9c02a44e1cf018e26537789c7b2
martinet <remymartinet.contact@gmail.com>
2024-09-23 11:53:38 +0200

    Added Copyright component with corresponding JavaScript and SCSS files.

---
+++ src/components/Common/Copyright/Copyright.jsx
@@ -0,0 +1,11 @@
+import "./Copyright.scss";
+
+const Copyright = () => {
+  const actualYear = new Date().getFullYear();
+  return (
+    <div className="copyright">
+      <p>{`© ${actualYear} by Margritt Martinet.`}</p>
+    </div>
+  );
+};
+export default Copyright;
