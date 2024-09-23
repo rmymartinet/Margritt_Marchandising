a2c8fa78f17e0190a0decc14e34824f3c6e77d8c
martinet <remymartinet.contact@gmail.com>
2024-09-23 11:10:41 +0200

    Updated OriginauxDetails component to display data

--- src/pages/Originaux/OriginauxDetails/OriginauxDetails.jsx
+++ src/pages/Originaux/OriginauxDetails/OriginauxDetails.jsx
@@ -2,7 +2,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { Flip } from "gsap/Flip";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useMemo, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import {
   IoIosArrowBack,
@@ -13,8 +13,8 @@ import { useNavigate, useParams } from "react-router-dom";
 import { Transition } from "../../../components/Animations/PageTransition/Transition.jsx";
 import Breadcrumb from "../../../components/Common/Breadcrumb/Breadcrumb.jsx";
 import InfoItem from "../../../components/Common/InfoItem.jsx";
-import { originauxData } from "../../../data/data.js";
-import "./OriginauxDetails.scss";
+import { useFilteredData } from "../../../components/hook/useFilteredData.jsx";
+import "../../Tirages/TiragesDetails/TiragesDetails.scss";
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(Flip);
@@ -29,50 +29,12 @@ const OriginauxDetails = () => {
   const { t } = useTranslation();
   const arrowRef = useRef(null);
   const titleRef = useRef(null);
-  const { id, size } = useParams(); // Récupère id et size depuis l'URL
+  const { id, size } = useParams(); // Retrieves id and size from URL
   const navigate = useNavigate();
 
-  // Filtre les œuvres selon la taille spécifiée
-  const originauxFiltered = originauxData.filter((item) => {
-    return item.dimension === size;
-  });
-
-  // Trouve l'œuvre sélectionnée par son id
-  const selectedImage = originauxFiltered.find(
-    (item) => item.id === Number(id)
-  );
-
-  const currentIndex = originauxFiltered.findIndex(
-    (item) => item.id === Number(id)
-  );
-
-  if (!selectedImage || currentIndex === -1) {
-    return <div>Œuvre non trouvée</div>;
-  }
-
-  const nextIndex = (currentIndex + 1) % originauxFiltered.length;
-  const nextItem = originauxFiltered[nextIndex];
   const [carouselIndex, setCarouselIndex] = useState(0);
 
-  // Créer un tableau des images associées à l'œuvre sélectionnée
-  const pictures = [
-    { src: selectedImage.imgWebp },
-    { src: selectedImage.img1 },
-    { src: selectedImage.img2 },
-    { src: selectedImage.img3 },
-    { src: selectedImage.img4 },
-  ].filter((picture) => picture.src); // Filtrer les images non définies
-
-  const allImages = useMemo(() => {
-    return Object.keys(selectedImage).filter((key) => key.includes("img"));
-  }, [selectedImage]);
-
-  /*---------------
-  Navigate to next item
-  ---------------- */
-  const handleNavigateNextOriginaux = (nextId) => {
-    navigate(`/originaux/${size}/${nextId}`); // Conserve `size` dans l'URL
-  };
+  // Creates an array of images associated with the selected artwork
 
   useGSAP(() => {
     gsap.to(titleRef.current, {
@@ -89,13 +51,13 @@ const OriginauxDetails = () => {
   /*---------------
   Carousel images
   ----------------*/
-  const handleNextSLide = () => {
-    setCarouselIndex((prevIndex) => (prevIndex + 1) % pictures.length);
+  const handleNextSlide = () => {
+    setCarouselIndex((prevIndex) => (prevIndex + 1) % selectedItem.length);
   };
 
-  const handlePrevSLide = () => {
+  const handlePrevSlide = () => {
     setCarouselIndex((prevIndex) =>
-      prevIndex === 0 ? pictures.length - 1 : prevIndex - 1
+      prevIndex === 0 ? selectedItem.length - 1 : prevIndex - 1
     );
   };
 
@@ -120,36 +82,67 @@ const OriginauxDetails = () => {
     });
   };
 
+  const { data } = useFilteredData(size);
+
+  const selectedItem = data.find((item) => item.id === id);
+
+  // Trouve l'index de l'élément sélectionné
+  const selectedIndex = data.findIndex((item) => item.id === id);
+
+  // Vérifie si l'index est valide
+  if (selectedIndex === -1) {
+    console.error("Invalid selectedIndex. Item not found.");
+    return;
+  }
+
+  // Calcule l'index suivant avec le modulo pour boucler
+  const nextIndex = (selectedIndex + 1) % data.length;
+
+  // Sélectionne l'élément suivant avec vérification
+  const nextItem = data[nextIndex];
+
+  if (!nextItem) {
+    console.error("Next item is undefined.");
+    return;
+  }
+
+  // Fonction pour naviguer vers l'élément suivant
+  const handleNavigateNextOriginaux = () => {
+    navigate(`/originals/${size}/${nextItem.id}`);
+  };
+
   return (
     <Transition>
-      <div className="orignaux-container">
+      <div className="details-container">
         <Breadcrumb />
         <div className="product-info">
           <div className="content-left">
-            {allImages.length > 2
-              ? pictures.map((picture, index) => (
-                  <div key={index} className="side-img">
-                    <img
-                      loading="lazy"
-                      src={pictures[carouselIndex].src}
-                      alt="image"
-                      placeholder="blur"
-                    />
-                  </div>
-                ))
-              : pictures.slice(0, 1).map(({ src }, index) => (
-                  <div key={index}>
-                    <div>
-                      <img src={src} alt="image" placeholder="blur" />
-                    </div>
-                  </div>
-                ))}
-            {allImages.length > 2 && (
+            {selectedItem.imageUrls.length > 2 ? (
+              <div className="side-img">
+                <img
+                  loading="lazy"
+                  src={selectedItem.imageUrls[carouselIndex]}
+                  alt="image"
+                  placeholder="blur"
+                />
+              </div>
+            ) : (
+              <div>
+                <div>
+                  <img
+                    src={selectedItem.imageUrls}
+                    alt="image"
+                    placeholder="blur"
+                  />
+                </div>
+              </div>
+            )}
+            {selectedItem.imageUrls.length > 2 && (
               <>
-                <div className="arrow-left" onClick={handlePrevSLide}>
+                <div className="arrow-left" onClick={handlePrevSlide}>
                   <IoIosArrowBack size={40} />
                 </div>
-                <div className="arrow-right" onClick={handleNextSLide}>
+                <div className="arrow-right" onClick={handleNextSlide}>
                   <IoIosArrowForward size={40} />
                 </div>
               </>
@@ -158,49 +151,51 @@ const OriginauxDetails = () => {
           <div className="content-right">
             <div className="title">
               <h1>
-                Série {selectedImage.serie} - {selectedImage.title}
+                Series {selectedItem.serie} - {selectedItem.title}
               </h1>
               <p>
-                Indisponible à la vente via le site. Me contacter pour en
-                discuter.
+                Not available for purchase through the website. Contact me to
+                discuss.
               </p>
             </div>
             <div className="details-infos">
               <InfoItem
-                label="Serie"
-                value={selectedImage.serie}
+                label="Series"
+                value={selectedItem.serie}
                 className="serie"
               />
               <InfoItem
-                label="Année"
-                value={selectedImage.date}
+                label="Year"
+                value={selectedItem.date}
                 className="date"
               />
               <InfoItem
-                label={t("originauxDetails.piece")}
-                value={selectedImage.piece}
+                label="Piece"
+                value={selectedItem.piece}
                 className="piece"
               />
               <InfoItem
                 label="Format"
-                value={selectedImage.format}
+                value={selectedItem.format}
                 className="format"
               />
               <InfoItem
-                label={t("originauxDetails.paper")}
-                value={selectedImage.papier}
+                label="Paper"
+                value={selectedItem.papier}
                 className="paper"
               />
             </div>
             <div className="buy-container">
-              <span className="contact">Me Contacter</span>
+              <span className="contact">
+                <a href="mailto:votreadresse@email.com">Contactez-moi</a>
+              </span>
               <div
                 className="next-work"
                 onMouseEnter={() => handleEnterHovered()}
                 onMouseLeave={() => handleEnterHovered()}
                 onClick={() => handleNavigateNextOriginaux(nextItem.id)}
               >
-                <span>Suivant</span>
+                <span>Next</span>
                 <div className="icon-container">
                   <div className="icon" ref={arrowRef}>
                     <IoIosArrowRoundForward />
